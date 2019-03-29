2b6e2185940c78e5638fe576712ca1dddb89fecd
Oliver Schafeld <oliver@schafeld.com>
2019-03-29 10:17:08 +0100

    import vue-resource

--- code/pwa/picturefoods-web-app/src/main.js
+++ code/pwa/picturefoods-web-app/src/main.js
@@ -5,7 +5,9 @@ import App from './App'
 import router from './router'
 import Vuefire from 'vuefire'
 import firebase from './services/firebase'
+import VueResource from 'vue-resource'
 
+Vue.use(VueResource)
 Vue.use(Vuefire)
 
 Vue.config.productionTip = false
