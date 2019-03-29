eac4631f10a56004d61f09dbd2fcff0bff3780c7
Oliver Schafeld <oliver@schafeld.com>
2019-03-29 19:02:21 +0100

    use working simple API

--- code/pwa/picturefoods-web-app/src/components/PostView.vue
+++ code/pwa/picturefoods-web-app/src/components/PostView.vue
@@ -22,7 +22,8 @@
 </template>
 
 <script>
-  import parse from 'xml-parser'
+  // import parse from 'xml-parser'
+
   export default {
     data () {
       return {
@@ -31,8 +32,10 @@
       }
     },
     mounted () {
-      this.$http.get('https://thecatapi.com/api/images/get?format=xml&results_per_page=1').then(response => {
-        this.catUrl = parse(response.body).root.children['0'].children['0'].children['0'].children['0'].content
+      /* this.$http.get('https://thecatapi.com/api/images/get?format=xml&results_per_page=1').then(response => {
+        this.catUrl = parse(response.body).root.children['0'].children['0'].children['0'].children['0'].content */
+      this.$http.get('https://api.thecatapi.com/v1/images/search').then(response => {
+        this.catUrl = response.body[0].url
       })
     },
     methods: {
