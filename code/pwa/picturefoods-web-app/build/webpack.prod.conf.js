c294bad96579e72d535b598769f099ae387b7424
Oliver Schafeld <oliver@schafeld.com>
2019-03-31 12:03:18 +0200

    add runtime caching (serve material design assets when offline)

--- code/pwa/picturefoods-web-app/build/webpack.prod.conf.js
+++ code/pwa/picturefoods-web-app/build/webpack.prod.conf.js
@@ -108,7 +108,20 @@ const webpackConfig = merge(baseWebpackConfig, {
       filename: 'service-worker.js',
       staticFileGlobs: ['dist/**/*.{js,html,css}'],
       minify: true,
-      stripPrefix: 'dist/'
+      stripPrefix: 'dist/',
+      runtimeCaching: [
+      {
+        urlPattern: /^https:\/\/fonts\.googleapis\.com\//,
+        handler: 'cacheFirst'
+      },
+      {
+        urlPattern: /^https:\/\/fonts\.gstatic\.com\//,
+        handler: 'cacheFirst'
+      },
+      {
+        urlPattern: /^https:\/\/code\.getmdl\.io\//,
+        handler: 'cacheFirst'
+      }]
     })
   ]
 })
