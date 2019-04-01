59646c047a40c77fa0d92ba60d92cd90e3b9c3c0
Oliver Schafeld <oliver@schafeld.com>
2019-04-01 21:14:14 +0200

    provide basic camera view/route

--- code/pwa/picturefoods-web-app/src/components/HomeView.vue
+++ code/pwa/picturefoods-web-app/src/components/HomeView.vue
@@ -17,6 +17,9 @@
     <router-link class="add-picture-button mdl-button mdl-js-button mdl-button--fab mdl-button--colored" to="/post">
       <i class="material-icons">add</i>
     </router-link>
+    <router-link class="take-picture-button mdl-button mdl-js-button mdl-button--fab mdl-button--colored" to="/camera">
+      <i class="material-icons">camera_alt</i>
+    </router-link>
   </div>
 </template>
 
@@ -93,5 +96,11 @@
     color: #fff;
     font-size: 14px;
     font-weight: bold;
+  }
+  .take-picture-button {
+    position: fixed;
+    right: 24px;
+    bottom: 90px;
+    z-index: 5;
   }
 </style>
