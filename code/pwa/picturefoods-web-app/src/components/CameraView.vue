fc1d14f23095cf914aa97553d4933742953a094f
Oliver Schafeld <oliver@schafeld.com>
2019-04-03 00:07:38 +0200

    add camera modal boilerplate

--- code/pwa/picturefoods-web-app/src/components/CameraView.vue
+++ code/pwa/picturefoods-web-app/src/components/CameraView.vue
@@ -1,11 +1,24 @@
 <template>
-    <div></div>
+    <div class="camera-modal">
+        <video ref="video" class="camera-stream"/>
+    </div>
 </template>
 
 <script>
-  export default {
-  }
 </script>
 
 <style scoped>
+    .camera-modal {
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        position: absolute;
+        background-color: white;
+        z-index: 10;
+    }
+    .camera-stream {
+        width: 100%;
+        max-height: 100%;
+    }
 </style>
