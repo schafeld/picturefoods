13e7257e7da94e83406f8f899b25f5fc02261cd6
Oliver Schafeld <oliver@schafeld.com>
2019-04-03 18:13:40 +0200

    initialize camera

--- code/pwa/picturefoods-web-app/src/components/CameraView.vue
+++ code/pwa/picturefoods-web-app/src/components/CameraView.vue
@@ -5,6 +5,16 @@
 </template>
 
 <script>
+  export default {
+    mounted () {
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then(mediaStream => {
+          this.$refs.video.srcObject = mediaStream
+          this.$refs.video.play()
+        })
+        .catch(error => console.error('getUserMedia() error:', error))
+    }
+  }
 </script>
 
 <style scoped>
