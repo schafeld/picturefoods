aebb367abd0cc2a0ad14df8ad7d8bfea7aac7b92
Oliver Schafeld <oliver@schafeld.com>
2019-04-04 07:52:05 +0200

    add camera button

--- code/pwa/picturefoods-web-app/src/components/CameraView.vue
+++ code/pwa/picturefoods-web-app/src/components/CameraView.vue
@@ -1,11 +1,20 @@
 <template>
     <div class="camera-modal">
         <video ref="video" class="camera-stream"/>
+        <div class="camera-modal-container">
+            <span @click="capture" class="take-picture-button take-picture-button mdl-button mdl-js-button mdl-button--fab mdl-button--colored">
+              <i class="material-icons">camera</i>
+            </span>
+        </div>
     </div>
 </template>
 
 <script>
   export default {
+    methods: {
+      capture () {
+      }
+    },
     mounted () {
       navigator.mediaDevices.getUserMedia({ video: true })
         .then(mediaStream => {
@@ -18,17 +27,27 @@
 </script>
 
 <style scoped>
-    .camera-modal {
-        width: 100%;
-        height: 100%;
-        top: 0;
-        left: 0;
-        position: absolute;
-        background-color: white;
-        z-index: 10;
-    }
-    .camera-stream {
-        width: 100%;
-        max-height: 100%;
-    }
+  .camera-modal {
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    position: absolute;
+    background-color: white;
+    z-index: 10;
+  }
+  .camera-stream {
+    width: 100%;
+    max-height: 100%;
+  }
+  .camera-modal-container {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    align-items: center;
+    margin-bottom: 24px;
+  }
+  .take-picture-button {
+    display: flex;
+  }
 </style>
