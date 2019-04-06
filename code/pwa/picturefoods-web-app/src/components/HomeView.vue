4989970783cc83792b4cc38c5d9605300ecd3cef
Oliver Schafeld <oliver@schafeld.com>
2019-04-06 11:49:47 +0200

    use new logo in home screen

--- code/pwa/picturefoods-web-app/src/components/HomeView.vue
+++ code/pwa/picturefoods-web-app/src/components/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <img src="../assets/logo-picturefoods-72x72.png" alt="Picturefoods PWA">
+    <img src="../assets/logo-picturefoods-300x80.png" alt="Picturefoods">
     <div class="mdl-grid">
       <div class="mdl-cell mdl-cell--3-col mdl-cell mdl-cell--1-col-tablet mdl-cell--hide-phone"></div>
       <div class="mdl-cell mdl-cell--6-col mdl-cell--4-col-phone">
