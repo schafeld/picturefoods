57ea834d48097137f1ca5876f96f8d90e73235f1
Oliver Schafeld <oliver@schafeld.com>
2019-04-07 16:06:45 +0200

    avoid text being selectable in PWA

--- code/pwa/picturefoods-web-app/src/App.vue
+++ code/pwa/picturefoods-web-app/src/App.vue
@@ -73,4 +73,11 @@ header span {
   box-sizing: border-box;
   padding-top: 16px;
 }
+
+div {
+  -webkit-user-select: none; /* Safari 3.1+ */
+  -moz-user-select: none; /* Firefox 2+ */
+  -ms-user-select: none; /* IE 10+ */
+  user-select: none; /* Standard syntax */
+ }
 </style>
