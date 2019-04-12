354ab43b17a57c5429933854e6adc1956d48742d
Oliver Schafeld <oliver@schafeld.com>
2019-04-12 13:25:37 +0200

    add basic firebase registration with email

--- code/pwa/picturefoods-web-app/src/components/RegisterView.vue
+++ code/pwa/picturefoods-web-app/src/components/RegisterView.vue
@@ -7,24 +7,46 @@
       </div>
       
       <div class="mdl-cell mdl-cell--3-col mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-        <input class="mdl-textfield__input" type="text" id="login">
-        <label class="mdl-textfield__label" for="login">Login</label>
+        <input class="mdl-textfield__input" type="text" id="login" v-model="login">
+        <label class="mdl-textfield__label" for="login">Login (E-Mail)</label>
       </div>
       <div class="mdl-cell mdl-cell--3-col mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-        <input class="mdl-textfield__input" type="text" id="password">
+        <input class="mdl-textfield__input" type="text" id="password" v-model="password">
         <label class="mdl-textfield__label" for="password">Password</label>
       </div>
-      <button class="mdl-cell mdl-cell--1-col">Sign up</button>
+      <button class="mdl-cell mdl-cell--1-col" @click="register">Sign up</button>
       <p>Or <router-link to="/">Log in</router-link></p>
     </div>
+    <div>Login {{login}}, password: {{password}} </div>
+    <div>Signup result: {{result}}</div>
   </div>
 </template>
 
 <script>
+  import firebase from 'firebase'
+
   export default {
-    methods: {},
+    methods: {
+      register () {
+        const that = this
+        firebase.auth().createUserWithEmailAndPassword(this.login, this.password).then(
+          function (user) {
+            console.log('Signup success')
+            that.result = 'Success'
+          },
+          function (err) {
+            console.log('Signup failure. ' + err.message)
+            that.result = 'Failure'
+          }
+        )
+      }
+    },
     data () {
-      return {}
+      return {
+        login: null,
+        password: null,
+        result: null
+      }
     },
     mounted () {}
   }
